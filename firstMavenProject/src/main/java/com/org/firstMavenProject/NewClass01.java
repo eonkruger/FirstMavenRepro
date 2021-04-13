ae2a21918e2f80c69b04f25004a57727b5f84a98
eonkruger <eonkruger@192.168.101.196>
2021-04-13 16:32:19 +0200

    Updated message

--- firstMavenProject/src/main/java/com/org/firstMavenProject/NewClass01.java
+++ firstMavenProject/src/main/java/com/org/firstMavenProject/NewClass01.java
@@ -5,11 +5,13 @@ public class NewClass01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//New class added to push code back
-		System.out.println("Hi team");
+		System.out.println("Hi team, updated my message");
 		
 		//Top-level right click, select maven and click on update project
 		//Right click, properties, build path, add variable and select M2_Repo
 		//Commit changes to local repository then push to remote
 		//Top-level right click, select teams, click on commit, select NewClass01 and click on the "+" button (new file to be committed) enter commit message "This is our first commit"
+		
+		//CHANGE MADE AND RECOMMITTED CHANGE TO PUSH
 	}
 }
