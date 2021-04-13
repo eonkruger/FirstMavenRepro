8112ef6ac9910f285911a7a56307e8a6e3f39cbe
eonkruger <eonkruger@192.168.101.196>
2021-04-13 16:17:09 +0200

    This is out first commit into master branch of this project

---
+++ firstMavenProject/src/main/java/com/org/firstMavenProject/NewClass01.java
@@ -0,0 +1,16 @@
+package com.org.firstMavenProject;
+
+public class NewClass01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//New class added to push code back
+		System.out.println("Hi team");
+		
+		//Top-level right click, select maven and click on update project
+		//Right click, properties, build path, add variable and select M2_Repo
+		//Commit changes to local repository then push to remote
+		//Top-level right click, select teams, click on commit
+	}
+
+}
